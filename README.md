ffcb7281314220ac1a4b0e77a24518204134ef1c
Juan Carlos <juan.arias@mobovida.com>
2019-08-06 09:50:56 -0500

    Added Read.me

---
+++ README.md
@@ -0,0 +1,79 @@
+# Ozine.js
+**Ozine.js** is small library that helps you use states to keep independent the logic from the view. Very similar in its integration to Jquery,  simply include the code in the project and start using it. And inspired on the principles of React. The idea is to bring the Joy of React where it might not be able to be integrated as easy as it is to integrate jQuery.
+
+The project relies on 3 pieces:
+
+ - **Ozine.DOM:** which works as an equivalent to JSX but is able to be plug
+   into a UI build in the backend, while completely being agnostic to
+   it.
+ -  **Ozine.addState:** A state manager which can be added into any object or component.
+ - **Ozine.Data:** As an equivalent of Redux to control the state of the entire app at  a single point without being so complicated to set up. And base on the principles used for state.
+
+**Ozine.js** is based on making things simple. The goal is to make code so simple that it’s distributed without being minify allowing people to read the code they are using.
+
+It’s so simple that the three pieces will be able to fit on a medium size t-shirt. In the case of our state manager it fits on a regular cup of coffee.
+
+## Ozine.DOM(html, UI)
+
+Our equivalent to JSX, we can send to the function a string with HTML valid syntax or a real node dom like *document.body* to analyze the entire page. On our regular html syntax we can add the attribute *ref*. In this attribute we can add a dot syntax variable name, when you do that node will be saved into the UI Object.
+
+**Params:**
+**Name:** Html
+**Type:** (html node/ string)
+**Description:** The html node that we will create from HTML syntax string or an already created node to be analyzed to extract references from.
+
+**Name:** UI
+**Type:** Object
+**Description:** Object where references to html nodes will be saved
+
+**Returns**
+**Type:** Html node/ fragment
+
+**For example**
+
+    <div ref=”person.title”></div>
+
+Will attach the html node at:
+*UI.person.title*
+
+So if we need to update the content of the DOM is as simple as:
+
+    UI.person.title.innerHTML.= "Hello World";
+
+
+## Ozine.addState(object)
+
+This is our equivalent to state in React, this function will add the setState function to object sent to it. It will also call **onState** with the changes done to the state before each frame (think of it as being called every x time to sync all the changes, just the way that React.js does it)
+
+**Params:**
+**Name:** object
+**Type:** (object)
+**Description:** Object which setState and onState will be added to.
+
+## Ozine.setState(partialState)
+
+Very similar to React’s equivalent we will be sending a partialState. This is the part of the state we want to be overridden. The state will be merged when requestAnimationFrame is called. This will allow to merge multiple updates to the state at the same time, similar to the way React does.
+
+**Params:**
+**Name:** partialState
+**Type:** (object)
+**Description:** Object with the properties to update the state with.
+
+## Ozine.onState(state_delta)
+
+We will wait for the next time the browser will try to repaint by waiting via using requestAnimationFrame. After that we will call onState in case it has been defined in the object and if a change of state has been requested via setState. Here we will differ from React’s approach: we will send as a param all the changes that have been requested via setState. This will create a nice dynamic when we are updating the UI since we are not using a virtual dom.
+
+**Example**
+var app= {};
+Ozine.addState(app);
+// We print on the page the changes done to the state, in this case both will
+// be apply at the same time so the result will be
+// { a: 1, b: 2 };
+app.onState = function(state){
+  document.body.innerHTML=JSON.stringify(state)
+};
+app.setState({a:1});
+app.setState({b:2});
+
+**Example using setState and Ozine.DOM to build UI**
+[https://codepen.io/anon/pen/KOVdKZ?editors=0010](https://codepen.io/anon/pen/KOVdKZ?editors=0010)
--- src/ozine.core.js
+++ src/ozine.core.js
@@ -1,154 +1,111 @@
 var Ozine = {};
-Ozine.DOM = function (html, scope, cleanHTML) {
-    var node = html;
-    if (html.constructor === String) {
-        node = document.createElement('div');
-        cleanHTML && (html = html.replace(/>\s*?</gim, '><'));
-        node.innerHTML = html;
-    }
-    (function toScope(node, scope) {
-        var children = node.children || [];
-        for (var iChild = 0; iChild < children.length; iChild++) {
-            var name = children[iChild].getAttribute('var') || children[iChild].getAttribute('ref');
-            if (children[iChild].getAttribute('className')){
-                children[iChild].setAttribute('class', children[iChild].getAttribute('className'))
-            }
-            var names = (name || '').split('.');
-            var obj = scope;
-            while (names.length > 1) {
-                var _property = names.shift();
-                if (!obj.hasOwnProperty(_property)) {
-                    obj[_property] = {};
-                }
-                obj = obj[_property];
-            }
-            (names.length == 1) && (obj[names[0]] = children[iChild]);
-            toScope(children[iChild], scope);
-        }
-    })(node, scope || {});
-    if (html.constructor == String && node.childNodes.length > 1) {
-        var fragment = document.createDocumentFragment();
-        while (node.childNodes.length > 0) {
-            if (fragment.append) {
-                fragment.append(node.childNodes[0]);
-            } else {
-                fragment.appendChild(node.childNodes[0]);
-            }
-        }
-        return fragment;
-    }
-    return (html.constructor == String ? node.childNodes[0]: node);
-}
 Object.extend = function(destination, source){
   for (var property in source) {
-      destination[property] = source[property];
+    destination[property] = source[property];
   }
   return destination;
 };
-
-
-
-
-
-
-
-
-
-
-
-Ozine.StateManager = {};
-Ozine.StateManager.init = function(){
-    var self = this;
-    if (!this.__stateManager) {
-        this.__stateManager = {pending: [], pendingPipe:[], onState: function(){
-            var pipe = self.__stateManager.pending;
-            var pendingPipe = self.__stateManager.pendingPipe;
-            var new_data  = {};
-            while (pipe.length > 0) {
-                new_data = Object.extend(new_data, pipe.shift().data);
-            }
-            while (pendingPipe.length > 0) {
-                var pipe = pendingPipe.shift().data;
-                for (var property in pipe){
-                    if (!new_data[property]){
-                        new_data[property] = [pipe[property]];
-                    } else if (new_data[property].constructor === Array){
-                        new_data[property].push(pipe[property])
-                    } else {
-                        new_data[property] = [pipe[property]];
-                    }
-                }
-            }
-            self.state = Object.extend(Object.extend({}, self.state), new_data);
-            self.__stateManager.requestUpdate = false;
-            self.onPreState && self.onPreState(new_data);
-            self.onState && self.onState(new_data);
-        }}
-    }
-    if (!this.__stateManager.requestUpdate) {
-        this.__stateManager.requestUpdate = true;
-        window.requestAnimationFrame(this.requestStateUpdate);
-    }
-}
-Ozine.StateManager.setStatePipe = function(data){
-    this.state = this.state || {};
-    this.init();
-    this.__stateManager.pendingPipe.push({data:data});
-}
-Ozine.StateManager.setState = function(data) {
-  this.state = this.state || {};
-  this.init();
-  this.__stateManager.pending.push({data:data});
-}
-Ozine.addState = function(element){
-  Object.extend(element, Ozine.StateManager);
-  element.state = element.state || {};
-  element.requestStateUpdate = function(){element.__stateManager.onState()};
-}
-Ozine.DataStore = function(){
-  this.stores = {};
-}
-Ozine.DataStore.prototype.createSingleStore = function(id){
-  if (!this.stores[id]) {
-      this.stores[id] = {subscribers:[], listeners:[]};
-      this.stores[id].onState = function(new_state){
-          for (var iSub = 0; iSub < this.subscribers.length; iSub ++){
-              this.subscribers[iSub](new_state);
-          }
+Ozine.DOM = function (html, scope, cleanHTML) {
+  var node = html;
+  if (html.constructor === String) {
+    node = document.createElement('div');
+    cleanHTML && (html = html.replace(/>\s*?</gim, '><'));
+    node.innerHTML = html;
+  }
+  (function toScope(node, scope) {
+    for (var iChild = 0; iChild < node.children.length; iChild++) {
+      var child = node.children[iChild];
+      var name = child.getAttribute('var') || child.getAttribute('ref');
+      var names = (name ? name.split('.') : []);
+      var obj = scope;
+      while (names.length > 1) {
+        var _property = names.shift();
+        if (!obj.hasOwnProperty(_property)) {
+          obj[_property] = {};
+        }
+        obj = obj[_property];
       }
-      Ozine.addState(this.stores[id]);
+      if (names[0] && names[0].search(/\[\]$/) > 0){
+        var arrayName = names[0].replace(/\[\]$/, '');
+        obj[arrayName] = obj[arrayName] ? obj[arrayName]: [];
+        obj[arrayName].push(child);
+        toScope(child, child);
+      } else{
+        (names.length == 1) && (obj[names[0].replace(/\*/, '')] = child);
+        toScope(child, (names[0] || '').match(/\*/) ? child : scope);
+      }
+    }
+  })(node, scope || {});
+  if (html.constructor == String && node.childNodes.length > 1) {
+    var fg = document.createDocumentFragment();
+    while (node.childNodes.length > 0) {
+      fg[(fg.append ? 'append' : 'appendChild')](node.childNodes[0]);
+    }
+    return fg;
+  }
+  return (html.constructor == String ? node.childNodes[0]: node);
+}
+Ozine.addState = function(el){
+  el.state = el.state || {};
+  var _state = {pending: [], onState: function(){
+    var data  = {};
+    while (_state.pending.length > 0) {
+      data = Object.extend(data, _state.pending.shift().data);
+    }
+    el.state = Object.extend(Object.extend({}, el.state), data);
+    _state.requestUpdate = false;
+    el.onState && el.onState(data);
+  }}
+  el.setState = function(data) {
+    if (!_state.requestUpdate) {
+      _state.requestUpdate = true;
+      window.requestAnimationFrame(_state.update);
+    }
+    _state.pending.push({data:data});
+  }
+  _state.update = function(){_state.onState();};
+}
+Ozine.Data = { stores:{} };
+Ozine.Data.createSingleStore = function (id) {
+  if (!this.stores[id]) {
+    this.stores[id] = { subscribers: [] };
+    this.stores[id].onState = function (new_state) {
+      for (var iSub = 0; iSub < this.subscribers.length; iSub++) {
+        this.subscribers[iSub](new_state);
+      }
+    };
+    Ozine.addState(this.stores[id]);
   }
   return this.stores[id];
-}
-Ozine.DataStore.prototype.getData = function(id){
-  return (this.stores[id]) ? this.stores[id].state : null;
-}
-Ozine.DataStore.prototype.getStore = function(id){
-return this.createSingleStore(id);
-}
-Ozine.DataStore.prototype.setState = function(id, data){
-  this.getStore(id).setState(data);
-}
-Ozine.DataStore.prototype.setStatePipe = function(id, data){
-    this.getStore(id).setStatePipe(data);
-}
-Ozine.DataStore.prototype.unsubscribe = function(id, callback){
-  var callbacks = this.stores[id].subscribers
-  for (var iCall = callbacks.length - 1 ; iCall >= 0; iCall--){
-      if (callbacks === callback){
-          callbacks.splice(iCall, 1)
-      }
+};
+Ozine.Data.getState = function (id) {
+  return (this.stores[id]) ? this.stores[id].state : {};
+};
+Ozine.Data.setState = function (id, data) {
+  this.createSingleStore(id).setState(data);
+};
+Ozine.Data.unsubscribe = function (id, callback) {
+  const callbacks = this.stores[id].subscribers;
+  for (var iCall = callbacks.length - 1; iCall >= 0; iCall--) {
+    (callbacks === callback) && callbacks.splice(iCall, 1);
   }
-}
-Ozine.DataStore.prototype.subscribe = function(id, callback){
+};
+Ozine.Data.subscribe = function (id, callback) {
   this.createSingleStore(id);
+  const callbacks = this.stores[id].subscribers;
+  for (var iCall = callbacks.length - 1; iCall >= 0; iCall--) {
+    if (callbacks === callback) {
+      return false;
+    }
+  }
   this.stores[id].subscribers.push(callback);
-}
-Ozine.DataStore.prototype.clear = function(id) {
+  callback(this.stores[id].state || {});
+};
+Ozine.Data.clear = function (id) {
   for (var property in this.stores) {
-      if (property.match('^' + id + '.')) {
-          delete this.stores[property];
-      }
+    if (property.match(`^${id}.`)) {
+      delete this.stores[property];
+    }
   }
   delete this.stores[id];
-}
+};
